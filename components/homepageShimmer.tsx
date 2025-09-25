880ed79c5175d781ee57d6956b1f64204ba38dc5
Arun <arun.rana9756@gmail.com>
2025-09-25 13:24:16 +0530

    miner change

--- components/homepageShimmer.tsx
+++ components/homepageShimmer.tsx
@@ -3,21 +3,21 @@ export default function HomePageShimmer() {
     [...Array(8)].map((_, i) => (
           <div
             key={i}
-            className="h-[25rem] sm:h-96 justify-self-center w-96 sm:w-72 rounded-lg bg-gray-700 overflow-hidden"
+            className="h-[25rem] sm:h-96 justify-self-center w-96 sm:w-72 rounded-lg bg-gray-300 dark:bg-gray-700 overflow-hidden"
           >
-            <div className="h-60 sm:h-52 bg-gray-600 animate-pulse"></div>
+            <div className="h-60 sm:h-52 bg-gray-200 dark:bg-gray-600 animate-pulse"></div>
             <div className="p-2 transform sm:translate-y-6 translate-y-3">
               <div className="h-7">
-                <div className="bg-gray-500 animate-pulse h-6 w-28 justify-self-center rounded-sm"></div>
+                <div className="bg-gray-100 dark:bg-gray-500 animate-pulse h-6 w-28 justify-self-center rounded-sm"></div>
               </div>
                <div className="h-7">
-                <div className="bg-gray-500 animate-pulse h-6 w-52 rounded-sm"></div>
+                <div className="bg-gray-100 dark:bg-gray-500 animate-pulse h-6 w-52 rounded-sm"></div>
               </div>
                <div className="h-7">
-                <div className="bg-gray-500 animate-pulse h-6 w-52 rounded-sm"></div>
+                <div className="bg-gray-100 dark:bg-gray-500 animate-pulse h-6 w-52 rounded-sm"></div>
               </div>
                <div className="h-7">
-                <div className="bg-gray-500 animate-pulse h-6 w-52 rounded-sm"></div>
+                <div className="bg-gray-100 dark:bg-gray-500 animate-pulse h-6 w-52 rounded-sm"></div>
               </div>
             </div>
           </div>
